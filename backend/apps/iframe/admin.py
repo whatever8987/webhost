d7c1eba4b77003efad6f49b350d2a691bdfd05f7
whatever8987 <166012561+whatever8987@users.noreply.github.com>
2025-05-22 04:57:32 -0700

    Delete backend directory

--- backend/apps/iframe/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Business, Type  # Changed Game to Business
-
-admin.site.register(Business)  # Changed Game to Business
-admin.site.register(Type)
